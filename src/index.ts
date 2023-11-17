5d686e937fdd39989dafdc8478a221b49d93ff74
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-17 18:23:51 +0300

    feat: expose all component types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export * from "./components";
-export { type ArticleProps } from "./components/ui/Article/interfaces";
-export { type BodyProps } from "./components/ui/Body/interfaces";
-export { type HeadingProps } from "./components/ui/Heading/interfaces";
-export { type ListProps } from "./components/ui/List/interfaces";
 export * from "./helpers";
 export { FontVariants, TextColors } from "./types/typography";
